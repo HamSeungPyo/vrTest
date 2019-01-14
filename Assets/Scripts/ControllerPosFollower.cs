8b86e0633924459fe3920680e8d94713c8866f95
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-01-15 03:52:23 +0900

    이동시스템 베타버전
    컨트롤러 모아서 위아래로 움직일시
    헤드셋 위치 기준으로
    2개의 컨트롤러 중앙 위치의 방향으로 이동

---
+++ Assets/Scripts/ControllerPosFollower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPosFollower : MonoBehaviour
+{
+    public GameObject controller;
+	void Update ()
+    {
+        transform.position = controller.transform.position;
+    }
+}
--- Assets/Scripts/MovingManager.cs
+++ Assets/Scripts/MovingManager.cs
@@ -1,20 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class MovingManager : MonoBehaviour
 {
-    public GameObject[] controller = new GameObject[2];
+    public GameObject[] controllerFollower = new GameObject[2];
     public GameObject moveAxis;
-	void Start ()
-    {
-		
-	}
-	
-	void Update ()
-    {
-        Vector3[] controllerPos = { controller[0].transform.localPosition, controller[1].transform.localPosition };
-        
+    public GameObject headAxis;
 
+    [Header("수정 가능한것")]
+    public float frictionValue = 2;
+    public float accelerationTime = 2;
+    public float moveSpeed = 5;
+    public float maxFindingRange_AxisY = 0.15f;
+    public float maxFindingRange_AxisX = 0.3f;
+    public float maxFindingRange__AxisZ = 0.1f;
+    public float maxFindedToControllerRange = 0.4f;
+    public float minAccelerationRange = 2.5f;
+
+    [Header("가속도 값(참고용)")]
+    public float move = 0;
+
+    float[] saveLength = new float[2];
+    float movingTime = 0;
+    bool[] bMovementValueSwitching = new bool[2];
+    bool bMovementFunctionSwitching = false;
+    
+    void Start ()
+    {
+        
+    }
+
+    void Update()
+    {
+        float controllerAxisX = (controllerFollower[0].transform.position.x + controllerFollower[1].transform.position.x) / 2;
+        float controllerAxisY = (controllerFollower[0].transform.position.z + controllerFollower[1].transform.position.z) / 2;
+        Vector3 axis = new Vector3(controllerAxisX, 0, controllerAxisY);
+        Vector3 targetPos = new Vector3(axis.x, headAxis.transform.position.y, axis.z);
+        headAxis.transform.LookAt(targetPos);
+        moveAxis.transform.eulerAngles = headAxis.transform.eulerAngles;
+
+        Vector3[] controllerPos = { controllerFollower[0].transform.localPosition, controllerFollower[1].transform.localPosition };
+        MovingControl(ref controllerPos);
+
+        transform.Translate(moveAxis.transform.forward * (move * moveSpeed) * Time.deltaTime);
+
+    }
+    void MovingControl(ref Vector3[] controllerPos)
+    {
+        float valueX = Mathf.Abs(controllerPos[0].x - controllerPos[1].x);
+        float valueY = Mathf.Abs(controllerPos[0].y - controllerPos[1].y);
+        if (!bMovementFunctionSwitching)
+        {
+            float valueZ = Mathf.Abs(controllerPos[0].z - controllerPos[1].z);
+            if (valueX < maxFindingRange_AxisX&& valueY < maxFindingRange_AxisY&& valueZ< maxFindingRange__AxisZ)
+            {
+                bMovementFunctionSwitching = true;
+            }
+            move = 0;
+            saveLength[0]= saveLength[1] = 0;
+        }
+        else
+        {
+            if (valueX > maxFindedToControllerRange)
+            {
+                bMovementFunctionSwitching = false;
+            }
+            float acceleration = AccelerationCalculator(valueY);
+            
+            if (acceleration > minAccelerationRange)
+            {
+                move = 1;
+            }
+            else
+            {
+                if (move >= 0.2f)
+                {
+                    move -= Time.deltaTime* frictionValue;
+                }
+                else
+                {
+                    move = 0;
+                }
+            }
+            //Debug.Log(acceleration);
+        }
+    }
+    float AccelerationCalculator(float moveDistance)
+    {
+        if (saveLength[0] == 0 && saveLength[1] == 0)
+        {
+            saveLength[1] = moveDistance;
+            saveLength[0] = moveDistance;
+            return 0;
+        }
+        if (saveLength[1] < moveDistance)
+        {
+            saveLength[1] = moveDistance;
+            bMovementValueSwitching[1] = true;
+        }
+        else if (bMovementValueSwitching[1] && saveLength[0] != moveDistance)
+        {
+            saveLength[0] = moveDistance;
+            movingTime = 0;
+            bMovementValueSwitching[1] = false;
+        }
+
+        if (saveLength[0] > moveDistance)
+        {
+            saveLength[0] = moveDistance;
+            bMovementValueSwitching[0] = true;
+        }
+        else if (bMovementValueSwitching[0] && saveLength[1] != moveDistance)
+        {
+            saveLength[1] = moveDistance;
+            movingTime = 0;
+            bMovementValueSwitching[0] = false;
+        }
+        movingTime += Time.deltaTime * accelerationTime;
+
+        float movementMeanValue = Mathf.Abs(saveLength[0] - saveLength[1]);
+        return (float)(movementMeanValue / movingTime);
     }
 }
