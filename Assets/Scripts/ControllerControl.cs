827376fe0b428b2f8d9d75b5836788e46669a403
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-01-14 18:35:36 +0900

    VR 이동방식 작업중

---
+++ Assets/Scripts/ControllerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerControl : MonoBehaviour
+{
+    //bool bCollideCheck
+
+    void Start ()
+    {
+		
+	}
+	void Update ()
+    {
+		
+	}
+    private void OnTriggerStay(Collider coll)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MovingManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingManager : MonoBehaviour
+{
+    public GameObject[] controller = new GameObject[2];
+    public GameObject moveAxis;
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Vector3[] controllerPos = { controller[0].transform.localPosition, controller[1].transform.localPosition };
+        
+
+    }
+}
