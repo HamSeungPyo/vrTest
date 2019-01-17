2f4a52d247eda6d174098103e66cda0d1ce9fdc6
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-01-17 20:04:28 +0900

    소화기 호수 작업중

---
+++ Assets/Scripts/FireHeadFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireHeadFollow : MonoBehaviour
+{
+    public GameObject[] tube = new GameObject[2];
+    public GameObject axis;
+    public GameObject fireHead;
+    GameObject controller;
+    bool bBasic=true;
+    void Update()
+    {
+        tube[0].SetActive(bBasic);
+        tube[1].SetActive(!bBasic);
+        if (controller == null)
+        {
+            bBasic = true;
+            return;
+        }
+        bBasic = false;
+        fireHead.transform.position = controller.transform.position;
+        fireHead.transform.eulerAngles = controller.transform.eulerAngles;
+    }
+    public void StartFollow(GameObject con)
+    {
+        controller = con;
+    }
+    public void StopFollow()
+    {
+        controller = null;
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public GameObject lookAt;
+    void Update ()
+    {
+        transform.LookAt(lookAt.transform);
+	}
+}
